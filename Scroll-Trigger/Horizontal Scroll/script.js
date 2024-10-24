538ef38476edfa80bc7594783b9094c809bbd975
realsahilsaini <thisissahilsaini@gmail.com>
2024-10-24 20:05:49 +0530

    Horizontal Scroll

---
+++ Scroll-Trigger/Horizontal Scroll/script.js
@@ -0,0 +1,13 @@
+gsap.to('#page2 h1',{
+  transform: 'translateX(-130%)',
+  scrollTrigger:{
+    trigger: '#page2',
+    scroller: 'body',
+    markers: true,
+    start: 'top 0%',
+    end: 'top -100%',
+    scrub: 4,
+    pin: true,
+  }
+})
+
