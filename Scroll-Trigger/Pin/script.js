093baf00257280dcacffe746650926fe88554357
realsahilsaini <thisissahilsaini@gmail.com>
2024-10-25 15:15:59 +0530

    Pin udated

--- Scroll-Trigger/Pin/script.js
+++ Scroll-Trigger/Pin/script.js
@@ -10,3 +10,24 @@ gsap.to('#page2 #main-img',{
     pin: true,
   }
 })
+
+
+gsap.to('#page2 h1',{
+  opacity: "100%",
+
+  scrollTrigger:{
+    trigger: '#page2',
+    scroller: 'body',
+    markers: true,
+    start: 'top 0%',
+    end: 'top -30%',
+    scrub: 1,
+    // pin: true,
+  }
+}
+)
+
+
+
+
+
